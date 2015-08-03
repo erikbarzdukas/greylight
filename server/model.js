b77d85de71ca71194dca8fb54495fdaedf9f1bbf
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-03 15:31:07 -0700

    Connect to mongo and define File model in model.js

---
+++ server/model.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/greylight');
+
+exports.File = mongoose.model('File' {
+  filename: String,
+  digest: String,
+  location: String,
+  VTreport: String,
+});
