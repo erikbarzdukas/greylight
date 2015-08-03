e6f73fd5649a75faf4f3c6ef4e5ab353e7db38eb
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-03 16:18:32 -0700

    Fixed typo in params

--- server/model.js
+++ server/model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/greylight');
 
-exports.File = mongoose.model('File' {
+exports.File = mongoose.model('File', {
   filename: String,
   digest: String,
   location: String,
