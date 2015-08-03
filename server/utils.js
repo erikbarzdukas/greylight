a75851cf87776530325593e6e1cc58ffdeb2e10d
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-03 16:18:58 -0700

    Call the store method once hash finishes

--- server/utils.js
+++ server/utils.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var crypto = require('crypto');
+var model = require('./model');
 
 /**
  * Create a hash from a file input
+ * then pass it to exports.store
  */
-exports.hash = function(file, hashAlgo){
+exports.hash = function(file, cb){
 
   var shasum = crypto.createHash('sha256');
-  var f = fs.ReadStream(file);
+  var f = fs.ReadStream(file.path);
 
   f.on('data', function(d){
     shasum.update(d);
@@ -15,7 +17,30 @@ exports.hash = function(file, hashAlgo){
 
   f.on('end', function(){
     var digest = shasum.digest('hex');
-    console.log(digest + ' ' + file);
+    console.log(digest + ' ' + file.path);
+    exports.store(digest, file, cb);
   });
 
+}
+
+/**
+ * Store file upload in mongodb
+ * and generate necessary info
+ * expects input param file to be
+ * file object from multer
+ */
+exports.store = function(digest, file, cb){
+  var File = new model.File({
+    filename: file.originalname,
+    digest: digest,
+    location: file.path
+  });
+
+  File.save(function(err){
+    if(err){
+      cb(err);
+    } else {
+      cb(null);
+    }
+  });
 }
