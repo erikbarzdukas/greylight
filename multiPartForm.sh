c089a3db5b092d1054c77c0814d986ba97ec6983
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-03 15:14:33 -0700

    Pointed test script at test zip

--- multiPartForm.sh
+++ multiPartForm.sh
@@ -1,1 +1,1 @@
-curl -v -F filedata=@/home/bronky/Programming/HackReactor/2015-06-mvp/evilfile.txt http://localhost:3000/api
+curl -v -F filedata=@/home/bronky/Programming/HackReactor/2015-06-mvp/evil.zip http://localhost:3000/api
