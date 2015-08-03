2511347624893899be5e16fb47b9ac6942934c38
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-03 16:59:02 -0700

    Added getByDigest to serve info for a particular digest:

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,14 +1,33 @@
 var utils = require('../utils');
 var model = require('../model');
 
+/* Return JSON of all files in DB */
 exports.get = function(req, res, next){
 
   model.File.find(function(err, files){
-    res.json(files);
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(files);
+    }
   });
 }
 
+/* Return JSON of all files that have digest param */
+exports.getByDigest = function(req, res, next){
+  
+  model.File.find({ digest: req.params.digest}, function(err, files){
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(files);
+    }
+  });
+}
+
+/* Handle upload of a file */
 exports.post = function(req, res, next){
+  
   utils.store(req.file, function(err){
     if(err){
       console.log(err);
