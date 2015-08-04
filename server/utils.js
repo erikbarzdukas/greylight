c128151aa5c68f66c6594251de0554878053fe8e
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-03 20:01:10 -0700

    Added storeScan

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var crypto = require('crypto');
 var model = require('./model');
+var Promise = require('bluebird');
 
 /**
  * Create a hash from a file input
@@ -36,6 +37,27 @@ exports.storeFile = function(file, cb){
       cb(err);
     } else {
       cb(null);
+    }
+  });
+}
+
+
+/**
+ * Stores scans -> updates if scan exists
+ * Expects a scan object that matches Virus Total docs
+ */
+/* Promisified way to retrieve scan docs */
+var storeScan = function(scan){
+  var promise = model.Scan.find({digest : scan.sha256}).exec();
+  promise.then(function(docs){
+    if(docs.length < 1) {
+      var newScan = new Scan({
+        scan: scan
+      });
+      newScan.save();
+    } else {
+      docs[0].scan = scan;
+      docs[0].save();
     }
   });
 }
