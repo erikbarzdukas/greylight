f13bd09cfb5773f5051f7a77ad88d4096eddfb62
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 15:00:03 -0700

    Added call to populate file collection

--- server/client/collections/fileCollection.js
+++ server/client/collections/fileCollection.js
@@ -2,4 +2,23 @@ var FileCollection = Backbone.Collection.extend({
   url: '/api/files',
   model: FileModel,
 
+  addFiles: function() {
+    $.get(
+        '/api/files',
+        function(files) {
+          for(var i = 0; i < files.length; i++) {
+            this.add({
+              filename: files[i].filename,
+              digest: files[i].digest,
+              location: files[i].location,
+              VTreport: files[i].VTreport
+            });
+          }
+        }.bind(this);
+    );
+  },
+
+  intialize: function() {
+    this.addFiles()
+  }
 });
