b39ecfd4163921fa4a0ffd231419d5336b7b15a7
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 16:42:54 -0700

    Added ability to download files

--- server/client/views/fileView.js
+++ server/client/views/fileView.js
@@ -1,9 +1,8 @@
 var FileView = Backbone.View.extend({
   class: 'file',
 
-  template: _.template('<p><%=filename%> <%=digest%><p>'),
+  template: _.template('<p><%=filename%> <%=digest%> <a href="http://127.0.0.1:3000/<%=download%>">Download</a><p>'),
   render: function() {
-    console.log(this);
     this.$el.append(this.template(this.model.attributes));
   },
 
