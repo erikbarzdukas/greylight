ba6be9f34fa2c1d9bdab7df7598d5dc11c1b28de
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 16:21:49 -0700

    Basic app view

--- server/client/views/appView.js
+++ server/client/views/appView.js
@@ -1,2 +1,11 @@
 var AppView = Backbone.View.extend({
+  render: function() {
+    this.$el.append(this.filesView.$el);
+    $('body').append(this.$el);
+  },
+
+  initialize: function() {
+    this.filesView = new FilesView({collection: this.model.files});
+    this.render();
+  } 
 });
