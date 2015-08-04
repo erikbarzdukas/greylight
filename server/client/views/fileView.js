144dfffef31b560be0f5d7da43d4a83d53dc85dd
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 16:22:14 -0700

    File view renders correctly

--- server/client/views/fileView.js
+++ server/client/views/fileView.js
@@ -1,2 +1,15 @@
 var FileView = Backbone.View.extend({
+  class: 'file',
+
+  template: _.template('<p><%=filename%> <%=digest%><p>'),
+  render: function() {
+    console.log(this);
+    this.$el.append(this.template(this.model.attributes));
+  },
+
+  initialize: function() {
+    this.render();
+  }
+
+
 });
