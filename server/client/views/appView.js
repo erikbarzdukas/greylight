91c90744ef87ed1be67ef0b1c1771642e7fa080a
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 17:22:21 -0700

    append views to respective divs and include scansView

--- server/client/views/appView.js
+++ server/client/views/appView.js
@@ -1,11 +1,13 @@
 var AppView = Backbone.View.extend({
   render: function() {
-    this.$el.append(this.filesView.$el);
+    $('#f').append(this.filesView.$el);
+    $('#s').append(this.scansView.$el);
     $('body').append(this.$el);
   },
 
   initialize: function() {
     this.filesView = new FilesView({collection: this.model.files});
+    this.scansView = new ScansView({collection: this.model.scans});
     this.render();
   } 
 });
