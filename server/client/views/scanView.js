14568f30c13a33883f62bbd1a92d8903bdbe0bb7
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 17:50:27 -0700

    Quick mock up of scan findings by AV

--- server/client/views/scanView.js
+++ server/client/views/scanView.js
@@ -2,11 +2,18 @@ var ScanView = Backbone.View.extend({
   class: 'scan',
 
   template: _.template('<p><%=sha256%> <a href="<%=permalink%>" target="_blank">Report</a></p>'),
-  
+  scansTemplate: _.template('<p><ul><li><b>Antivirus: </b><%=av%></li> <li><b>Detected:</b> <%=detected%> </li><li><b>Filename:</b> <%=result%></li></ul></p>'),
+
   render: function() {
     if(this.model.attributes.sha256 !== undefined) {
       this.$el.append(this.template(this.model.attributes));
-      this.$el.append(JSON.stringify(this.model.attributes.scans));
+      for(var key in this.model.attributes.scans) {
+        this.$el.append(this.scansTemplate({
+          av: key,
+          detected: this.model.attributes.scans[key]["detected"],
+          result: this.model.attributes.scans[key]["result"],
+        }));
+      }
     }
   },
 
