b4a3c04c6e27dc21a5362d5fe7d5b49c345dedab
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 17:06:55 -0700

    boned out scans collection

--- server/client/collections/scanCollection.js
+++ server/client/collections/scanCollection.js
@@ -1,4 +1,28 @@
 var ScanCollection = Backbone.Collection.extend({
-  url: '/api/scans',
+  url: 'http://127.0.0.1:3000/api/scans',
   model: ScanModel,
+
+  addScans: function() {
+    $.get(this.url,
+        function(scans) {
+          for(var i = 0; i < scans.length; i++) {
+            this.add({
+              scanId: scans[i].scan_id,
+              md5: scans[i].md5,
+              sha1: scans[i].sha1,
+              sha256: scans[i].sha256,
+              scanDate: scans[i].scan_date,
+              positives: scans[i].positives,
+              total: scans[i].total,
+              scans: scans[i].scans,
+              permalink: scans[i].permalink,
+            });
+          }
+        }.bind(this)
+    );
+  },
+
+  initialize: function() {
+    this.addScans();
+  }
 });
