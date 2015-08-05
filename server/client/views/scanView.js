45ef1b7968ab509d71ea2348c781ab7c7505fb28
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-04 17:35:21 -0700

    Don't display scan if not a valid scan, display stringified JSON scans object for now

--- server/client/views/scanView.js
+++ server/client/views/scanView.js
@@ -1,10 +1,13 @@
 var ScanView = Backbone.View.extend({
   class: 'scan',
 
-  template: _.template('<p><%=sha256%> <a href="<%=permalink%>" target="_blank">Report</a> <p><%=scans%></p></p>'),
+  template: _.template('<p><%=sha256%> <a href="<%=permalink%>" target="_blank">Report</a></p>'),
   
   render: function() {
-    this.$el.append(this.template(this.model.attributes));
+    if(this.model.attributes.sha256 !== undefined) {
+      this.$el.append(this.template(this.model.attributes));
+      this.$el.append(JSON.stringify(this.model.attributes.scans));
+    }
   },
 
   initialize: function() {
